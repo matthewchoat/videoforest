7c624256659067888a90aabcb5dc0fe327eecad4
matthewchoat <matthewchoat@gmail.com>
2019-07-30 03:16:23 +0000

    lesson 20 refactoring

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,6 +1,6 @@
 class Instructor::CoursesController < ApplicationController
   before_action :authenticate_user!
-
+  before_action :require_authorized_for_current_course, only: [:show]
     def new
       @course = Course.new
     end
@@ -15,10 +15,20 @@ class Instructor::CoursesController < ApplicationController
     end
 
     def show
-      @course = Course.find(params[:id])
     end
     
     private
+
+    def require_authorized_for_current_course
+      if current_course.user != current_user
+        render plain: "Unauthorized", status: :unauthorized
+      end
+    end
+
+    helper_method :current_course
+    def current_course
+      @current_course ||= Course.find(params[:id])
+    end
 
     def course_params
       params.require(:course).permit(:title, :description, :cost)
