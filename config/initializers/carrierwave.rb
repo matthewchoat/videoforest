d902a67744c332f545200d1df071d4c93db69e1e
matthewchoat <matthewchoat@gmail.com>
2019-08-02 04:36:29 +0000

    lesson 22complete

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,8 @@
+# config/initializers/carrierwave.rb
+
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
+  config.fog_public = false
   config.fog_credentials = {
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
